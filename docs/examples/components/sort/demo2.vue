9597b8381526a31931f9821abf5d1cd755806c4c
nixiaoyu <1149062971@qq.com>
2024-11-22 12:33:16 +0800

    feat: update docs

---
+++ docs/examples/components/sort/demo2.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { MtSort } from '@mortise-tenon/components';
+import { ref } from 'vue';
+
+const list = ref([2, 7, 9, 6, 4, 3, 1]);
+</script>
+
+<template>
+  序号5、8不在排序修改范围中
+  <h6>使用v-if隐藏元素</h6>
+  <MtSort :sort-list="list">
+    <div>
+      <div v-for="i in 5" :key="i" :sort-key="i">
+        {{ i }}
+      </div>
+    </div>
+    <div :sort-key="6">
+      6
+    </div>
+    <div>
+      <div v-if="false" :sort-key="7">
+        7
+      </div>
+      <div>
+        8
+      </div>
+      <div :sort-key="9">
+        9
+      </div>
+    </div>
+  </MtSort>
+  <h6>使用v-show隐藏元素</h6>
+  <MtSort :sort-list="list">
+    <div>
+      <div v-for="i in 5" :key="i" :sort-key="i">
+        {{ i }}
+      </div>
+    </div>
+    <div :sort-key="6">
+      6
+    </div>
+    <div>
+      <div v-show="false" :sort-key="7">
+        7
+      </div>
+      <div>
+        8
+      </div>
+      <div :sort-key="9">
+        9
+      </div>
+    </div>
+  </MtSort>
+</template>
+
+<style scoped>
+div {
+  display: inline-block;
+}
+</style>
--- docs/zh/hooks/use-throttle-control.md
+++ docs/zh/hooks/use-throttle-control.md
@@ -2,18 +2,18 @@
 
 节流控制。有效控制程序执行的间隔时间。
 
-### 通过节流函数控制
+## 通过节流函数控制
 
 ::: demo
 hooks/use-throttle-control/demo1
 :::
 
-### 通过节流状态控制
+## 通过节流状态控制
 
 ::: demo
 hooks/use-throttle-control/demo2
 :::
 
-### 源码
+## 源码
 
 [源代码](https://github.com/nixwai/mortise-tenon/blob/main/packages/hooks/use-throttle-control/index.ts)
