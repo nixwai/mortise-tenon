1cc27f5c404cec218482f50123891d9ef11c0dad
nixiaoyu <1149062971@qq.com>
2025-01-07 18:52:40 +0800

    chore(deps): update deps

--- docs/examples/presets/button/3d.vue
+++ docs/examples/presets/button/3d.vue
@@ -12,19 +12,25 @@
     <button class="btn btn-3d btn-bright">
       bright
     </button>
+    <button class="btn btn-3d btn-deep">
+      deep
+    </button>
   </div>
   <div class="mt-4">
-    <button disabled class="btn btn-3d shadow">
+    <button disabled class="btn btn-3d">
       ghost
     </button>
-    <button disabled class="btn btn-3d shadow btn-pale">
+    <button disabled class="btn btn-3d btn-pale">
       pale
     </button>
-    <button disabled class="btn btn-3d shadow btn-soft">
+    <button disabled class="btn btn-3d btn-soft">
       soft
     </button>
-    <button disabled class="btn btn-3d shadow btn-bright">
+    <button disabled class="btn btn-3d btn-bright">
       bright
+    </button>
+    <button disabled class="btn btn-3d btn-deep">
+      deep
     </button>
   </div>
   <div class="mt-4">
@@ -39,6 +45,9 @@
     </button>
     <button class="btn btn-3d rounded-full aspect-square btn-bright">
       b
+    </button>
+    <button class="btn btn-3d rounded-full aspect-square btn-deep">
+      d
     </button>
   </div>
 </template>
