f9c9d9619efe240305bd8fa7394c81ff1a6a4fe8
nixiaoyu <1149062971@qq.com>
2025-02-27 18:33:11 +0800

    feat: update tool files

--- packages/core/build-tool/gulp-build.ts
+++ packages/core/build-tool/gulp-build.ts
@@ -1,10 +1,16 @@
-import { parallel, series } from 'gulp';
-import { toolOutput } from '../paths';
-import { delPath, run } from '../tasks';
+import { resolve } from 'node:path';
+import { series } from 'gulp';
+import { copyFiles, delPath, run } from '../tasks';
+import { toolOutput, toolRoot } from './paths';
 
 export default series(
   () => delPath(toolOutput),
-  parallel(
-    () => run('vite build'),
+  () => run('vite build'),
+  () => copyFiles(
+    toolOutput,
+    [
+      resolve(toolRoot, 'README.md'),
+      resolve(toolRoot, 'package.json'),
+    ],
   ),
 );
