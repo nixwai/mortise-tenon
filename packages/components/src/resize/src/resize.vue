25c7207edaa19e74643234e871802c73980710eb
nixiaoyu <1149062971@qq.com>
2025-05-23 16:20:09 +0800

    feat: 完善功能

--- packages/components/src/resize/src/resize.vue
+++ packages/components/src/resize/src/resize.vue
@@ -5,26 +5,24 @@ import { ref } from 'vue';
 const props = withDefaults(defineProps<ResizeProps>(), { disabled: false });
 
 const contentRef = ref<HTMLDivElement>();
-
-let containWidth = '0';
-
 function handlePointerdown(event: PointerEvent) {
+  if (props.disabled) {
+    return;
+  }
   if (!contentRef.value) {
     return;
   }
-  contentRef.value.setPointerCapture(event.pointerId); // 设置鼠标捕获(之后的事件捕获会作用在当前元素上)
-  if (!props.disabled) {
-    const startX = event.clientX;
-    const clientWidth = contentRef.value?.clientWidth || 0; // 容器的宽度
-    contentRef.value.onpointermove = (e) => {
-      if (contentRef.value?.style) {
-        const endX = e.clientX;
-        const width = clientWidth - (startX - endX);
-        containWidth = `${width}px`;
-        contentRef.value.style.width = containWidth;
-      }
-    };
-  }
+  contentRef.value.setPointerCapture(event.pointerId);
+  const startX = event.clientX;
+  const clientWidth = contentRef.value?.clientWidth || 0;
+  contentRef.value.onpointermove = (e) => {
+    if (contentRef.value?.style) {
+      const endX = e.clientX;
+      const width = clientWidth - (startX - endX);
+      contentRef.value.style.width = `${width}px`;
+    }
+  };
+  contentRef.value.onpointerup = handlePointerup;
 }
 
 function handlePointerup(event: PointerEvent) {
@@ -32,12 +30,12 @@ function handlePointerup(event: PointerEvent) {
     return;
   }
   contentRef.value.onpointermove = null;
-  contentRef.value.releasePointerCapture(event.pointerId); // 释放鼠标捕获
+  contentRef.value.releasePointerCapture(event.pointerId);
 }
 </script>
 
 <template>
-  <div ref="contentRef" @pointerup="handlePointerup" @pointerdown="handlePointerdown">
+  <div ref="contentRef" @pointerdown="handlePointerdown">
     <slot />
   </div>
 </template>
