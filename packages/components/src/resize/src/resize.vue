42dccdb5615c181dcf94b23f19157ec0e1bc3dff
nixiaoyu <1149062971@qq.com>
2025-06-04 18:25:55 +0800

    feat: 添加各个方向滑块控制大小

--- packages/components/src/resize/src/resize.vue
+++ packages/components/src/resize/src/resize.vue
@@ -5,32 +5,104 @@ import { ref } from 'vue';
 const props = withDefaults(defineProps<ResizeProps>(), { disabled: false });
 
 const contentRef = ref<HTMLDivElement>();
-/** 开始拖拽容器 */
-function beginResizeContent(event: PointerEvent) {
-  if (props.disabled) { return; }
-  if (!contentRef.value) { return; }
-  contentRef.value.setPointerCapture(event.pointerId);
-  const startX = event.clientX;
-  const clientWidth = contentRef.value.clientWidth || 0;
-  contentRef.value.onpointermove = async (e) => {
-    const endX = e.clientX;
-    const width = clientWidth - (startX - endX);
-    contentRef.value!.style.width = `${width}px`;
-  };
-  contentRef.value.onpointerup = overResizeContent;
+
+let translateX = 0;
+let translateY = 0;
+
+/** 左侧拖拽容器 */
+function resizeLeftDirection(beginEvent: PointerEvent) {
+  const startX = beginEvent.clientX;
+  beginResizeContent(beginEvent, (moveEvent, { clientWidth }) => {
+    const moveX = startX - moveEvent.clientX;
+    const width = clientWidth + moveX;
+    if (width > 0) {
+      contentRef.value!.style.width = `${width}px`;
+      contentRef.value!.style.transform = `translate(${translateX - moveX}px, ${translateY}px)`;
+    }
+    else {
+      contentRef.value!.style.width = `${-width}px`;
+    }
+  });
 }
 
-/** 结束拖拽容器 */
-function overResizeContent(event: PointerEvent) {
+/** 右侧拖拽容器 */
+function resizeRightDirection(beginEvent: PointerEvent) {
+  const startX = beginEvent.clientX;
+  beginResizeContent(beginEvent, (moveEvent, { clientWidth }) => {
+    const moveX = moveEvent.clientX - startX;
+    const width = clientWidth + moveX;
+    if (width > 0) {
+      contentRef.value!.style.width = `${width}px`;
+    }
+    else {
+      contentRef.value!.style.transform = `translate(${translateX + width}px, ${translateY}px)`;
+      contentRef.value!.style.width = `${-width}px`;
+    }
+  });
+}
+
+/** 上方拖拽容器 */
+function resizeTopDirection(beginEvent: PointerEvent) {
+  const startY = beginEvent.clientY;
+  beginResizeContent(beginEvent, (moveEvent, { clientHeight }) => {
+    const moveY = startY - moveEvent.clientY;
+    const height = clientHeight + moveY;
+    if (height > 0) {
+      contentRef.value!.style.height = `${height}px`;
+      contentRef.value!.style.transform = `translate(${translateX}px, ${translateY - moveY}px)`;
+    }
+    else {
+      contentRef.value!.style.height = `${-height}px`;
+    }
+  });
+}
+
+/** 下方拖拽容器 */
+function resizeBottomDirection(beginEvent: PointerEvent) {
+  const startY = beginEvent.clientY;
+  beginResizeContent(beginEvent, (moveEvent, { clientHeight }) => {
+    const moveY = moveEvent.clientY - startY;
+    const height = clientHeight + moveY;
+    if (height > 0) {
+      contentRef.value!.style.height = `${height}px`;
+    }
+    else {
+      contentRef.value!.style.height = `${-height}px`;
+      contentRef.value!.style.transform = `translate(${translateX}px, ${translateY + height}px)`;
+    }
+  });
+}
+
+/** 开始拖拽容器 */
+function beginResizeContent(
+  beginEvent: PointerEvent,
+  resizingContent: (moveEvent: PointerEvent, size: { clientWidth: number, clientHeight: number }) => void,
+) {
+  if (props.disabled) { return; }
   if (!contentRef.value) { return; }
+  const { clientWidth, clientHeight } = contentRef.value;
+  contentRef.value!.setPointerCapture(beginEvent.pointerId);
+  contentRef.value!.onpointermove = moveEvent => resizingContent(moveEvent, { clientWidth, clientHeight });
+  contentRef.value!.onpointerup = overResizeContent;
+}
+
+const translateValueReg = /translate\((.+)px,(.+)px\)/;
+/** 结束拖拽容器 */
+function overResizeContent(overEvent: PointerEvent) {
+  if (!contentRef.value) { return; }
+  translateX = Number(contentRef.value!.style.transform?.match(translateValueReg)?.[1] || 0);
+  translateY = Number(contentRef.value!.style.transform?.match(translateValueReg)?.[2] || 0);
   contentRef.value.onpointermove = null;
-  contentRef.value.releasePointerCapture(event.pointerId);
+  contentRef.value.releasePointerCapture(overEvent.pointerId);
 }
 </script>
 
 <template>
   <div ref="contentRef" class="mt-resize">
-    <div class="right-box" @pointerdown.stop="beginResizeContent" />
+    <div class="left-box" @pointerdown.stop.prevent="resizeLeftDirection" />
+    <div class="right-box" @pointerdown.stop.prevent="resizeRightDirection" />
+    <div class="top-box" @pointerdown.stop.prevent="resizeTopDirection" />
+    <div class="bottom-box" @pointerdown.stop.prevent="resizeBottomDirection" />
     <slot />
   </div>
 </template>
@@ -40,6 +112,15 @@ function overResizeContent(event: PointerEvent) {
   position: relative;
   overflow: visible;
 
+  .left-box {
+    position: absolute;
+    top: 4px;
+    left: -4px;
+    width: 8px;
+    height: calc(100% - 8px);
+    cursor: w-resize;
+  }
+
   .right-box {
     position: absolute;
     top: 4px;
@@ -47,6 +128,24 @@ function overResizeContent(event: PointerEvent) {
     width: 8px;
     height: calc(100% - 8px);
     cursor: e-resize;
+  }
+
+  .top-box {
+    position: absolute;
+    top: -4px;
+    left: 4px;
+    width: calc(100% - 8px);
+    height: 8px;
+    cursor: n-resize;
+  }
+
+  .bottom-box {
+    position: absolute;
+    bottom: -4px;
+    left: 4px;
+    width: calc(100% - 8px);
+    height: 8px;
+    cursor: s-resize;
   }
 }
 </style>
