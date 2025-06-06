be099c41e483b36a8fbb70b6d992252f0024ed66
nixiaoyu <1149062971@qq.com>
2025-06-06 15:02:01 +0800

    feat: 添加锁定纵横比功能

--- packages/components/src/resize/src/resize.ts
+++ packages/components/src/resize/src/resize.ts
@@ -5,7 +5,7 @@ export interface ResizeProps {
    */
   disabled?: boolean
   /**
-   * 定位位置，适用于元素脱离文档流时使用，可设置偏移量
+   * 使用定位，适用于元素脱离文档流时使用，可设置偏移量
    * @default false
    */
   positioned?: boolean | { x?: number, y?: number }
@@ -14,4 +14,19 @@ export interface ResizeProps {
    * @default ['right']
    */
   directions?: ('left' | 'right' | 'top' | 'bottom')[]
+  /**
+   * 锁定纵横比，directions至少需要设置两个方向横向与纵向方向的值
+   * @default false
+   */
+  lockAspectRatio?: boolean
 }
+
+/**
+ * 可调整的方向
+ */
+export type Direction = 'left' | 'right' | 'top' | 'bottom';
+
+/**
+ * 调整状态 'prepare'(准备) | 'moving'(移动) | 'idle'(静止)
+ */
+export type ResizeStatus = 'prepare' | 'moving' | 'idle';
--- packages/components/src/resize/src/resize.vue
+++ packages/components/src/resize/src/resize.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ResizeProps } from './resize';
+import type { ResizeProps, ResizeStatus } from './resize';
 import { computed, ref } from 'vue';
 
 defineOptions({ name: 'MtResize' });
@@ -10,31 +10,45 @@ const props = withDefaults(
     disabled: false,
     positioned: false,
     directions: () => ['right'],
+    lockAspectRatio: false,
   },
 );
 
+const emit = defineEmits<{
+  (e: 'resize', status: ResizeStatus): void
+}>();
+
 /** 元素实例 */
 const contentRef = ref<HTMLDivElement>();
-
+/** 是否自定义位置 */
 const isCustomPosition = typeof props.positioned === 'object';
 /** X轴位移 */
-let contentTranslateX = isCustomPosition ? props.positioned.x || 0 : 0;
+let cacheTranslateX = isCustomPosition ? props.positioned.x || 0 : 0;
 /** Y轴位移 */
-let contentTranslateY = isCustomPosition ? props.positioned.y || 0 : 0;
+let cacheTranslateY = isCustomPosition ? props.positioned.y || 0 : 0;
 
-/** 向前调整（往右或者往下） */
-function resizingForward(start: number, end: number, clientValue: number, offsetValue: number) {
-  const move = start - end;
-  const value = clientValue + move;
-  return value > 0 ? { value, offset: offsetValue - move } : { value, offset: offsetValue + clientValue };
+/** 调整状态 */
+const resizeStatus = ref<ResizeStatus>('idle');
+
+function updateResizeStatus(status: ResizeStatus) {
+  resizeStatus.value = status;
+  emit('resize', status);
 }
 
-/** 向后调整（往左或者往上） */
-function resizingBackward(start: number, end: number, clientValue: number, offsetValue: number) {
+type ResizingFn = (start: number, end: number, clientValue: number, offsetValue: number) => { value: number, offset: number };
+/** 向前调整（往右或者往下） */
+const resizingForward: ResizingFn = (start, end, clientValue, offsetValue) => {
   const move = start - end;
   const value = clientValue - move;
   return value > 0 ? { value, offset: offsetValue } : { value, offset: offsetValue + value };
-}
+};
+
+/** 向后调整（往左或者往上） */
+const resizingBackward: ResizingFn = (start, end, clientValue, offsetValue) => {
+  const move = start - end;
+  const value = clientValue + move;
+  return value > 0 ? { value, offset: offsetValue - move } : { value, offset: offsetValue + clientValue };
+};
 
 /** 设置宽度 */
 const setStyleWidth = props.positioned
@@ -54,112 +68,119 @@ const setStyletTransform = props.positioned
 /** 开始拖拽容器 */
 function beginResizeContent(
   beginEvent: PointerEvent,
-  resizingContent: (moveEvent: PointerEvent, clientSize: { clientWidth: number, clientHeight: number }) => void,
+  resizingContent: (moveEvent: PointerEvent, clientSize: { clientWidth: number, clientHeight: number, aspectRatio: number }) => void,
 ) {
   if (props.disabled) { return; }
   if (!contentRef.value) { return; }
   const { clientWidth, clientHeight } = contentRef.value;
+  const aspectRatio = clientWidth / clientHeight;
   contentRef.value.setPointerCapture(beginEvent.pointerId);
+  contentRef.value.onpointermove = (moveEvent) => {
+    resizingContent(moveEvent, { clientWidth, clientHeight, aspectRatio });
+    updateResizeStatus('moving');
+  };
   contentRef.value.onpointerup = overResizeContent;
-  contentRef.value.onpointermove = moveEvent => resizingContent(moveEvent, { clientWidth, clientHeight });
+  updateResizeStatus('prepare');
 }
 
 const translateValueReg = /translate\((.+)px,(.+)px\)/;
 /** 结束拖拽容器 */
 function overResizeContent(overEvent: PointerEvent) {
   if (!contentRef.value) { return; }
-  contentTranslateX = Number(contentRef.value.style.transform?.match(translateValueReg)?.[1] || 0);
-  contentTranslateY = Number(contentRef.value.style.transform?.match(translateValueReg)?.[2] || 0);
+  cacheTranslateX = Number(contentRef.value.style.transform?.match(translateValueReg)?.[1] || 0);
+  cacheTranslateY = Number(contentRef.value.style.transform?.match(translateValueReg)?.[2] || 0);
   contentRef.value.onpointermove = null;
   contentRef.value.releasePointerCapture(overEvent.pointerId);
+  updateResizeStatus('idle');
+}
+
+/** 调整宽度 */
+function resizingWidth(beginEvent: PointerEvent, resizingWidthFn: ResizingFn) {
+  const { clientX: startX } = beginEvent;
+  beginResizeContent(beginEvent, ({ clientX: endX }, { clientWidth }) => {
+    const { value: width, offset: translateX } = resizingWidthFn(startX, endX, clientWidth, cacheTranslateX);
+    setStyleWidth(width);
+    setStyletTransform(translateX, cacheTranslateY);
+  });
 }
 
 /** 选择调整左侧 */
 function handleResizeLeft(beginEvent: PointerEvent) {
-  const { clientX: startX } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX }, { clientWidth }) => {
-    const { value, offset } = resizingForward(startX, endX, clientWidth, contentTranslateX);
-    setStyleWidth(value);
-    setStyletTransform(offset, contentTranslateY);
-  });
+  resizingWidth(beginEvent, resizingBackward);
 }
 
 /** 选择调整右侧 */
 function handleResizeRight(beginEvent: PointerEvent) {
-  const { clientX: startX } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX }, { clientWidth }) => {
-    const { value, offset } = resizingBackward(startX, endX, clientWidth, contentTranslateX);
-    setStyleWidth(value);
-    setStyletTransform(offset, contentTranslateY);
+  resizingWidth(beginEvent, resizingForward);
+}
+
+/** 调整高度 */
+function resizingHeight(beginEvent: PointerEvent, resizingHeightFn: ResizingFn) {
+  const { clientY: startY } = beginEvent;
+  beginResizeContent(beginEvent, ({ clientY: endY }, { clientHeight }) => {
+    const { value: height, offset: translateY } = resizingHeightFn(startY, endY, clientHeight, cacheTranslateY);
+    setStyleHeight(height);
+    setStyletTransform(cacheTranslateX, translateY);
   });
 }
 
 /** 选择调整上方 */
 function handleResizeTop(beginEvent: PointerEvent) {
-  const { clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientY: endY }, { clientHeight }) => {
-    const { value, offset } = resizingForward(startY, endY, clientHeight, contentTranslateY);
-    setStyleHeight(value);
-    setStyletTransform(contentTranslateX, offset);
-  });
+  resizingHeight(beginEvent, resizingBackward);
 }
 
 /** 选择调整下方 */
 function handleResizeBottom(beginEvent: PointerEvent) {
-  const { clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientY: endY }, { clientHeight }) => {
-    const { value, offset } = resizingBackward(startY, endY, clientHeight, contentTranslateY);
-    setStyleHeight(value);
-    setStyletTransform(contentTranslateX, offset);
-  });
+  resizingHeight(beginEvent, resizingForward);
+}
+
+/** 调整高度与宽度 */
+function resizingWidthAndHeight(beginEvent: PointerEvent, resizingWidthFn: ResizingFn, resizingHeightFn: ResizingFn) {
+  const updateDom = (options: { startX: number, startY: number, endX: number, endY: number, clientWidth: number, clientHeight: number }) => {
+    const { value: width, offset: translateX } = resizingWidthFn(options.startX, options.endX, options.clientWidth, cacheTranslateX);
+    const { value: height, offset: translateY } = resizingHeightFn(options.startY, options.endY, options.clientHeight, cacheTranslateY);
+    setStyleWidth(width);
+    setStyleHeight(height);
+    setStyletTransform(translateX, translateY);
+  };
+
+  if (props.lockAspectRatio) {
+    // 固定比例时，宽度根据鼠标位置决定，高度的调整根据宽度的变化与元素宽高比例决定
+    const { clientX: startX } = beginEvent;
+    const dir = resizingWidthFn === resizingHeightFn ? 1 : -1; // 宽高调整的方向不是同向时，需要反向调整高度
+    beginResizeContent(beginEvent, ({ clientX: endX }, { clientWidth, clientHeight, aspectRatio }) => {
+      const startY = dir * startX / aspectRatio;
+      const endY = dir * endX / aspectRatio;
+      updateDom({ startX, startY, endX, endY, clientWidth, clientHeight });
+    });
+  }
+  else {
+    // 不固定比例时，宽高根据鼠标位置决定
+    const { clientX: startX, clientY: startY } = beginEvent;
+    beginResizeContent(beginEvent, ({ clientX: endX, clientY: endY }, { clientWidth, clientHeight }) => {
+      updateDom({ startX, startY, endX, endY, clientWidth, clientHeight });
+    });
+  }
 }
 
 /** 选择调整左上方 */
 function handleResizeLeftTop(beginEvent: PointerEvent) {
-  const { clientX: startX, clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX, clientY: endY }, { clientWidth, clientHeight }) => {
-    const { value: width, offset: translateX } = resizingForward(startX, endX, clientWidth, contentTranslateX);
-    const { value: height, offset: translateY } = resizingForward(startY, endY, clientHeight, contentTranslateY);
-    setStyleWidth(width);
-    setStyleHeight(height);
-    setStyletTransform(translateX, translateY);
-  });
+  resizingWidthAndHeight(beginEvent, resizingBackward, resizingBackward);
 }
 
 /** 选择调整右上方 */
 function handleResizeRightTop(beginEvent: PointerEvent) {
-  const { clientX: startX, clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX, clientY: endY }, { clientWidth, clientHeight }) => {
-    const { value: width, offset: translateX } = resizingBackward(startX, endX, clientWidth, contentTranslateX);
-    const { value: height, offset: translateY } = resizingForward(startY, endY, clientHeight, contentTranslateY);
-    setStyleWidth(width);
-    setStyleHeight(height);
-    setStyletTransform(translateX, translateY);
-  });
+  resizingWidthAndHeight(beginEvent, resizingForward, resizingBackward);
 }
 
 /** 选择调整左下方 */
 function handleResizeLeftBottom(beginEvent: PointerEvent) {
-  const { clientX: startX, clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX, clientY: endY }, { clientWidth, clientHeight }) => {
-    const { value: width, offset: translateX } = resizingForward(startX, endX, clientWidth, contentTranslateX);
-    const { value: height, offset: translateY } = resizingBackward(startY, endY, clientHeight, contentTranslateY);
-    setStyleWidth(width);
-    setStyleHeight(height);
-    setStyletTransform(translateX, translateY);
-  });
+  resizingWidthAndHeight(beginEvent, resizingBackward, resizingForward);
 }
 
 /** 选择调整右下方 */
 function handleResizeRightBottom(beginEvent: PointerEvent) {
-  const { clientX: startX, clientY: startY } = beginEvent;
-  beginResizeContent(beginEvent, ({ clientX: endX, clientY: endY }, { clientWidth, clientHeight }) => {
-    const { value: width, offset: translateX } = resizingBackward(startX, endX, clientWidth, contentTranslateX);
-    const { value: height, offset: translateY } = resizingBackward(startY, endY, clientHeight, contentTranslateY);
-    setStyleWidth(width);
-    setStyleHeight(height);
-    setStyletTransform(translateX, translateY);
-  });
+  resizingWidthAndHeight(beginEvent, resizingForward, resizingForward);
 }
 
 const isLeftDir = computed(() => props.directions.includes('left'));
@@ -172,19 +193,19 @@ const isBottomDir = computed(() => props.directions.includes('bottom'));
   <div
     ref="contentRef"
     class="mt-resize"
-    :style="isCustomPosition ? { transform: `translate(${contentTranslateX}px,${contentTranslateY}px)` } : {}"
+    :style="isCustomPosition ? { transform: `translate(${cacheTranslateX}px,${cacheTranslateY}px)` } : {}"
   >
     <template v-if="!disabled">
-      <div v-if="isLeftDir" class="left-box" @pointerdown.stop.prevent="handleResizeLeft" />
-      <div v-if="isRightDir" class="right-box" @pointerdown.stop.prevent="handleResizeRight" />
-      <div v-if="isTopDir" class="top-box" @pointerdown.stop.prevent="handleResizeTop" />
-      <div v-if="isBottomDir" class="bottom-box" @pointerdown.stop.prevent="handleResizeBottom" />
+      <div v-if="isLeftDir && !lockAspectRatio" class="left-box" @pointerdown.stop.prevent="handleResizeLeft" />
+      <div v-if="isRightDir && !lockAspectRatio" class="right-box" @pointerdown.stop.prevent="handleResizeRight" />
+      <div v-if="isTopDir && !lockAspectRatio" class="top-box" @pointerdown.stop.prevent="handleResizeTop" />
+      <div v-if="isBottomDir && !lockAspectRatio" class="bottom-box" @pointerdown.stop.prevent="handleResizeBottom" />
       <div v-if="isLeftDir && isTopDir" class="left-top-box" @pointerdown.stop.prevent="handleResizeLeftTop" />
       <div v-if="isRightDir && isTopDir" class="right-top-box" @pointerdown.stop.prevent="handleResizeRightTop" />
       <div v-if="isLeftDir && isBottomDir" class="left-bottom-box" @pointerdown.stop.prevent="handleResizeLeftBottom" />
       <div v-if="isRightDir && isBottomDir" class="right-bottom-box" @pointerdown.stop.prevent="handleResizeRightBottom" />
     </template>
-    <slot />
+    <slot :status="resizeStatus" />
   </div>
 </template>
 
